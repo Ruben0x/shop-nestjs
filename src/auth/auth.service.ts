87920422f486d08dab1ee3ddbc4ada74feeec1c0
ruben <r.alejandro.gn@gmail.com>
2025-01-28 15:14:36 -0300

    auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,14 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto, CreateUserDto } from './dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+
+import { compareSync, hashSync } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -16,14 +19,39 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: hashSync(password, 10),
+      });
 
       await this.userRepository.save(user);
 
+      delete user.password;
+
       return user;
+      //Todo: retornar JWT de acceso
     } catch (error) {
       this.handleDBErrors(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+
+    if (!user) throw new UnauthorizedException('Credentials are not valid');
+
+    if (!compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials are not valid');
+
+    return user;
+    //TODO: retornar JWT
   }
 
   private handleDBErrors(error: any): never {
